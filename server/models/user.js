b871f318377d5402d6bfe0ce54243beb3de17105
mostafa69d <mostafa69d@gmail.com>
2018-01-06 18:50:06 -0500

    feat: add user model

---
+++ server/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `${VALUE} is not an valid email`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+
+//hash the password with salt before saving the user
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(error, salt) {
+        if (error) {
+            return next(error);
+        }
+
+        bcrypt.hash(user.password, salt, null, function(error, hash) {
+            if (error) {
+                return next(error);
+            }
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(enteredPassword, callback) {
+    bcrypt.compare(enteredPassword, this.password, function(error, isMatch) {
+        if (error) {
+            return next(error);
+        }
+
+        callback(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', userSchema);
