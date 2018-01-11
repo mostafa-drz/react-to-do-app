3eaf264cab5265251f67c766a4335962d3d769a4
mostafa69d <mostafa69d@gmail.com>
2018-01-11 14:28:55 -0500

    refactor: make DateTimePicker component and replace

---
+++ frontend/src/components/DateTimePicker.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Datetime from "react-datetime";
+import "../stylesheets/react-datetime.css";
+import GoCalendar from "react-icons/lib/go/calendar";
+import * as moment from "moment";
+import '../stylesheets/dateTimePicker.css';
+
+class DateTimePicker extends Component {
+  constructor(props) {
+    super(props);
+    this._clearDate = this._clearDate.bind(this);
+    this.state = { date: null };
+  }
+
+  _handleDateTimeChange(e) {
+    this.setState({ date: e._d }, () => {
+      this.props.onChange(this.state.date);
+    });
+  }
+
+  _clearDate() {
+    this.setState({ date: null });
+  }
+
+  _renderDateTimeInput(props, openCalendar) {
+    function clear() {
+      props.onChange({ target: { value: "" } });
+    }
+    
+    return (
+      <div>
+        <GoCalendar
+          onClick={openCalendar}
+          className="dateTimePicker__calendarIcon"
+        />
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div className="dateTimePicker">
+        {this.props.value && (
+          <label>
+            {moment(this.props.value).format("MMM D,ddd h:mm a")}
+            <span
+              onClick={this._clearDate}
+              className="dateTimePicker__clearDateBtn"
+            >
+              X
+            </span>
+          </label>
+        )}
+        <Datetime
+          renderInput={this._renderDateTimeInput}
+          onChange={e => this._handleDateTimeChange(e)}
+          value={this.state.date}
+        />
+      </div>
+    );
+  }
+}
+
+export default DateTimePicker;
--- frontend/src/components/NewToDo.js
+++ frontend/src/components/NewToDo.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addToDoOnServer } from '../actions/todo';
 import '../stylesheets/newToDo.css';
-import Datetime from 'react-datetime'
-import '../stylesheets/react-datetime.css';
-import GoCalendar from 'react-icons/lib/go/calendar';
-import * as moment from 'moment';
+import DateTimePicker from './DateTimePicker';
+
 class NewToDo extends Component {
+
   constructor(props) {
     super(props);
     this.state = { description: "", date: null };
-    this._clearDate = this._clearDate.bind(this);
   }
 
   _handelInputChange(target) {
@@ -28,25 +26,13 @@ class NewToDo extends Component {
     await this.props.addToDoOnServer({ description, date });
     this.setState({ description: "", date: null });
   }
-  _renderDateTimeInput(props, openCalendar) {
-    function clear() {
-      props.onChange({ target: { value: "" } });
-    }
 
-    return (
-      <div>
-        <GoCalendar onClick={openCalendar} className="newtodo__calendarIcon" />
-      </div>
-    );
+
+  _handleDateTimeChange(value) {
+    this.setState({ date: value});
   }
 
-  _handleDateTimeChange(e) {
-    this.setState({ date: e._d });
-  }
 
-  _clearDate() {
-    this.setState({ date: null });
-  }
   render() {
     return <div className="row newtodo">
         <input type="text" placeholder="What else?..." id="newtodo__description" value={this.state.description} onKeyPress={e => {
@@ -54,19 +40,10 @@ class NewToDo extends Component {
           }} onChange={e => {
             this._handelInputChange(e.target);
           }} />
-        <div className="newtodo__date">
-          {this.state.date && <label>
-              {moment(this.state.date).format("MMM D,ddd h:mm a")}
-              <span onClick={this._clearDate} className="newtodo__clearDateBtn">
-                X
-              </span>
-            </label>}
-          <Datetime 
-          renderInput={this._renderDateTimeInput} 
-          onChange={e => this._handleDateTimeChange(e)} 
-          value={this.state.date} 
+          <DateTimePicker
+            onChange={(value)=>this._handleDateTimeChange(value)}
+            value={this.state.date}
           />
-        </div>
       </div>;
   }
 }
