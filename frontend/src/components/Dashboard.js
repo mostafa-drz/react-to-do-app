8d69b4a57b3b4a5206df59b191b054703bb4c6c2
mostafa69d <mostafa69d@gmail.com>
2018-01-14 13:37:58 -0500

    feat: add change view mode in dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -4,17 +4,67 @@ import ToDoList from './toDo/ToDoList';
 import {fetchToDos} from '../actions/todo';
 import NewToDo from './toDo/NewToDo';
 import '../stylesheets/dashboard.css';
-
+import Toggle from './Toggle';
+import Day from './Day';
+import MdFormatListBulleted from 'react-icons/lib/md/format-list-bulleted';
+import TiCalendarOutline from 'react-icons/lib/ti/calendar-outline';
+import RadioButton from './RadioButton';
 class Dashboard extends Component {
+  constructor(props){
+    super(props);
+    this._handleChangeView=this._handleChangeView.bind(this);
+    this.renderBasedOnView=this.renderBasedOnView.bind(this);
+  }
+
+  state={
+    view:'list'
+  }
   componentDidMount() {
     this.props.fetchToDos().catch((error)=>{
       console.log(error.message);
     });
   }
+
+
+  renderBasedOnView(){
+    switch (this.state.view) {
+      case 'list':
+         return <ToDoList key='todolist'/>;
+      case 'calendar':
+        return <Day key='day'/>
+    }
+  }
+
+  _handleChangeView(value){
+    this.setState({
+      view:value
+    })
+  }
   render() {
-    return <div className="container body dashboard">
-        <NewToDo />
-        <ToDoList />
+    return <div className="container dashboard">
+        <div className="row">
+          <RadioButton
+          buttons={[
+            {
+              id:'list',
+              label:'List View',
+              icon:<MdFormatListBulleted/>,
+              checked:true
+            },
+            {
+              id:'calendar',
+              label:'Calendar View',
+              icon:<TiCalendarOutline/>,
+              checked:false
+            }
+          ]}
+          onRadioChange={(id)=>{this._handleChangeView(id)}}
+          />
+        </div>
+        <div className="row">
+          <NewToDo key="newtodo" />
+        </div>
+        {this.renderBasedOnView()}
       </div>;
   }
 }
---
+++ frontend/src/components/Day.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class Day extends Component{
+    render(){
+        return(
+
+            <div>Day</div>
+        );
+    }
+}
+
+export default Day;
