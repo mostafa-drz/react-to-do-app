1353eae8df12e00ae4b24fc323ad52aad2361116
mostafa69d <mostafa69d@gmail.com>
2018-01-20 17:12:54 -0500

    bug: fix production bu on heroku

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require("passport");
 const app = express();
+const path = require('path');
+
 require('./services/db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,20 +13,19 @@ app.use(cors());
 
 app.use(passport.initialize());
 
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
 const authRoute = require('./routes/auth');
 app.use(authRoute);
 
 const todoRoutes = require('./routes/todo');
 app.use(todoRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../fontend/build', 'index.html'));
+});
+
 
 app.listen(process.env.PORT || 3001, () => {
     console.log('server is running');
