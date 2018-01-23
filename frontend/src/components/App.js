53c9a9f30246463b575bb5d2623b8144cff1ff45
mostafa69d <mostafa69d@gmail.com>
2018-01-23 15:31:21 -0500

    fix: google auth redirect

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../stylesheets/app.css';
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Redirect,Switch } from "react-router-dom";
 import Login from './auth/Login';
 import Signup from './auth/Signup';
 import RequireAuth from './auth/require_authentication_hoc';
@@ -13,13 +13,15 @@ class App extends Component {
       <BrowserRouter>
         <div className="body">
           <Header/>
-          <Route exact path="/">
-            <Redirect to='/dashboard' />
-          </Route>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/signup" component={Signup}/>
-          <Route path='/auth/google/callback' component={GoogleLogIn}/>
-          <Route exact path="/dashboard" component={RequireAuth(Dashboard)}/>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to='/dashboard' />
+            </Route>
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/signup" component={Signup}/>
+            <Route path='/auth/google/callback' component={GoogleLogIn}/>
+            <Route exact path="/dashboard" component={RequireAuth(Dashboard)}/>
+          </Switch>
         </div>
       </BrowserRouter>);
   }
