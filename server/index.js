7c16646035c7dad268b86942b441c67e2daa13cb
mostafa69d <mostafa69d@gmail.com>
2018-02-08 17:53:42 -0500

    fix: pkj error

--- server/index.js
+++ server/index.js
@@ -30,7 +30,6 @@ app.use(todoRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../frontend/build'));
-    const path = require('path');
     app.get('*', (req, res) => {
         console.log(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
         res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
