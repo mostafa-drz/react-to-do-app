d403cab98e3d47d06cc8a4c9a51110ccd531bb61
mostafa69d <mostafa69d@gmail.com>
2018-02-09 17:29:52 -0500

    unregister for register workers

--- server/frontend/src/index.js
+++ server/frontend/src/index.js
@@ -3,19 +3,19 @@ import 'materialize-css/dist/js/materialize.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import axios from 'axios';
 import { applyMiddleware, createStore,compose } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 import {isUserAuthorized} from './utils/helpers';
 import reducers from './reducers';
-import {AUTH} from './actions/types'
+import {AUTH} from './actions/types';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers,{},composeEnhancers(applyMiddleware(reduxThunk)));
 
 store.dispatch({type:AUTH,auth:isUserAuthorized()});
-window.axios = axios;
+unregister();
 ReactDOM.render(
     <Provider store={store}> 
         <App/>
