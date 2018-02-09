f6a6fb9227d386260b55709fbc0c5e53e62d6868
mostafa69d <mostafa69d@gmail.com>
2018-02-09 11:28:11 -0500

    test on server router

--- server/index.js
+++ server/index.js
@@ -29,11 +29,6 @@ app.use(todoRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
-
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
 }
 
 
